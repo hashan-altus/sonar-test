28656f150381c8a6d4d3a07bc2b3e24ac015d15c
Hashan Jay` <62950833+hashan-altus@users.noreply.github.com>
2021-01-26 11:29:11 -0500

    Update user.service.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,4 +3,11 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class UserService {
   constructor() { }
+  
+  private HelpLink(): string {
+    for(let i=0; i<20; i++){
+     console.log(i); 
+    }
+    return 'This is a help Link'; 
+  }
 }
